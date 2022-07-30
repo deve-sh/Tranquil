13e4617a16283f9575cbc721cca605b5ae9d0615
Devesh Kumar <devesh2027@gmail.com>
2022-07-30 15:14:15 +0530

    Added setup for socket.io

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import filesRouter from "./routes/files";
+import { createServer } from "http";
+import socket from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const socketServer = socket(server);
+
 const PORT = process.env.PORT || 8080;
 
 app.use("*", (req, _, next) => {
@@ -11,4 +16,4 @@ app.use("*", (req, _, next) => {
 
 app.use("/files", filesRouter);
 
-app.listen(PORT, () => console.log("App running at port: ", PORT));
+server.listen(PORT, () => console.log("App running at port: ", PORT));
