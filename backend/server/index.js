2afbaced4f19d61b7be410d333bd02d3f9e3497b
Devesh Kumar <devesh2027@gmail.com>
2022-07-30 15:49:29 +0530

    Projects router integrated

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,26 +1,30 @@
 import express from "express";
-import filesRouter from "./routes/files";
 import { createServer } from "http";
 import socket from "socket.io";
 import cors from "cors";
 
-import setupSocketControllers from "./controllers/socket";
+// Routers
+import filesRouter from "./routes/files";
+import projectsRouter from "./routes/projects";
 
 const app = express();
 const server = createServer(app);
-const socketServer = socket(server, { cors: { origin: "*" } });
 
-const PORT = process.env.PORT || 8080;
-
+// Middlewares
 app.use("*", cors());
-
 app.use("*", (req, _, next) => {
 	console.log("Request at: ", req.url);
 	next();
 });
 
+// Socket Server and Controllers
+import setupSocketControllers from "./controllers/socket";
+const socketServer = socket(server, { cors: { origin: "*" } });
 setupSocketControllers(socketServer);
 
+// Routes
 app.use("/files", filesRouter);
+app.use("/projects", projectsRouter);
 
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log("App running at port: ", PORT));
