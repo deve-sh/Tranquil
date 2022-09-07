1faaeba1919b4e8219e89c78a0b2c86b30dce320
Devesh Kumar <devesh2027@gmail.com>
2022-09-07 19:09:38 +0530

    feat: Added body parsing for server

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,21 @@ const cors = require("cors");
 
 const setupMongoDBConnection = require("../common/db");
 
-// Routers
-const filesRouter = require("./routes/files");
-const projectsRouter = require("./routes/projects");
-
 const app = express();
 const server = createServer(app);
 
 // Middlewares
 app.use("*", cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("*", (req, _, next) => {
 	console.log("Request at: ", req.url);
 	next();
 });
+
+// Routers
+const filesRouter = require("./routes/files");
+const projectsRouter = require("./routes/projects");
 
 // Socket Server and Controllers
 const setupSocketControllers = require("./controllers/socket");
