dd94434037c8eaa9b81481d4cb0d13c49d60be75
Devesh Kumar <devesh2027@gmail.com>
2022-09-07 17:43:23 +0530

    feat: Moved MySQL to PG

--- common/db/index.js
+++ common/db/index.js
@@ -1,5 +1,9 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
-const db = new Sequelize(process.env.DB_URL, { dialect: "mysql" });
+const db = new Sequelize(process.env.DB_URL, {
+	dialect: "postgres",
+	dialectModule: pg,
+});
 
 module.exports = db;
