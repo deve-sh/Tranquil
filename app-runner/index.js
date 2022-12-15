531f1eeece2ae3f3505f9877e408ad44cc556623
Devesh Kumar <devesh2027@gmail.com>
2022-12-15 22:08:26 +0530

    app-runner: Added app runner script

---
+++ app-runner/index.js
@@ -0,0 +1,71 @@
+// Script for running an app on an EC2 Instance/Docker Container
+// Syntax for invocation: node ./index.js "projectId" "installCommand?" "startCommand?"
+
+require("dotenv").config();
+
+const { spawn } = require("child_process");
+const { io } = require("socket.io-client");
+
+const {
+	BROADCAST_TO_PROJECT,
+	PROJECT_INSTANCE_STATES,
+} = require("../common/socketTypes");
+
+const projectId = process.argv[2] || "";
+const installCommand = process.argv[3] || "npm install";
+const startCommand = process.argv[4] || "npm run start";
+
+const broadCastSecret = process.env.PROJECT_SOCKET_BROADCAST_SECRET;
+
+if (projectId && installCommand && startCommand && broadCastSecret) {
+	const socket = io(process.env.MAIN_BACKEND_URL);
+
+	socket.on("connect", () => {
+		// Connected to backend via socket.
+		// Spawn app install and runner processes.
+		const appRunningProcess = spawn(`${installCommand} && ${startCommand}`, {
+			shell: true,
+		});
+		appRunningProcess.stdout.on("data", (data) => {
+			const logString = data.toString().trim();
+			if (logString.length) {
+				// Send to server via socket for it to broadcast to project clients.
+				socket.emit(BROADCAST_TO_PROJECT, {
+					projectId,
+					broadCastSecret: process.env.PROJECT_SOCKET_BROADCAST_SECRET,
+					data: { type: PROJECT_INSTANCE_STATES.STDOUT, log: logString },
+				});
+			}
+		});
+
+		appRunningProcess.stderr.on("data", (data) => {
+			const logString = data.toString();
+			if (logString.trim().length) {
+				// Send to server via socket for it to broadcast to project clients.
+				socket.emit(BROADCAST_TO_PROJECT, {
+					projectId,
+					broadCastSecret: process.env.PROJECT_SOCKET_BROADCAST_SECRET,
+					data: { type: PROJECT_INSTANCE_STATES.STDERR, log: logString },
+				});
+			}
+		});
+
+		appRunningProcess.on("close", (code, signal) => {
+			if (code || signal) {
+				// Errored Exit
+				socket.emit(BROADCAST_TO_PROJECT, {
+					projectId,
+					broadCastSecret: process.env.PROJECT_SOCKET_BROADCAST_SECRET,
+					data: { type: PROJECT_INSTANCE_STATES.CRASHED, code },
+				});
+			} else {
+				// Clean Exit
+				socket.emit(BROADCAST_TO_PROJECT, {
+					projectId,
+					broadCastSecret: process.env.PROJECT_SOCKET_BROADCAST_SECRET,
+					data: { type: PROJECT_INSTANCE_STATES.STOPPED },
+				});
+			}
+		});
+	});
+}
--- common/socketTypes.js
+++ common/socketTypes.js
@@ -2,3 +2,11 @@ module.exports.PROJECT_INIT_UPDATE = "project-initialization-update";
 module.exports.PROJECT_SHUT_DOWN = "project-shut-down";
 module.exports.LISTEN_TO_PROJECT = "listen-to-project";
 module.exports.BROADCAST_TO_PROJECT = "broadcast-to-project";
+
+// Project Instance Event Types
+module.exports.PROJECT_INSTANCE_STATES = {
+	STDOUT: "project-instance-stdout",
+	STDERR: "project-instance-stderr",
+	STOPPED: "project-instance-stopped",
+	CRASHED: "project-instance-crashed",
+};
