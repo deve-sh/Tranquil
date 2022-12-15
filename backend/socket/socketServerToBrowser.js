44cac6acdf75200ad73740de3836f067c5366491
Devesh Kumar <devesh2027@gmail.com>
2022-12-15 21:54:29 +0530

    backend: Added socket broadcast secret

--- backend/socket/socketServerToBrowser.js
+++ backend/socket/socketServerToBrowser.js
@@ -1,7 +1,8 @@
 const { server: expressServer } = require("../express-server");
 
 const socketIO = require("socket.io");
-const { LISTEN_TO_PROJECT } = require("./types");
+
+const { LISTEN_TO_PROJECT, BROADCAST_TO_PROJECT } = require("./types");
 
 const socketServerToBrowser = socketIO(expressServer, {
 	cors: { origin: "*" },
@@ -13,6 +14,22 @@ socketServerToBrowser.on("connection", (client) => {
 		// Make the device socket connection join a room specific to this project id.
 		// This will be used to broadcast project specific logs and updates in real-time
 		client.join(event.projectId);
+		console.log("Joined client:", client.id, "to project", event.projectId);
+	});
+
+	client.on(BROADCAST_TO_PROJECT, (event) => {
+		// This is mainly for the app runner script from EC2 Instance/Docker Container to send logs
+		// and signals to clients subscribing to the project's updates.
+		if (
+			!event ||
+			!event.projectId ||
+			!event.broadCastSecret ||
+			!event.broadCastSecret === process.env.PROJECT_SOCKET_BROADCAST_SECRET ||
+			!event.data ||
+			!typeof event.data === "object"
+		)
+			return;
+		socketServerToBrowser.to(event.projectId, event.data);
 	});
 	client.on("end", () => client.disconnect(0));
 });
--- backend/socket/types.js
+++ backend/socket/types.js
@@ -1,3 +1,4 @@
 module.exports.PROJECT_INIT_UPDATE = "project-initialization-update";
 module.exports.PROJECT_SHUT_DOWN = "project-shut-down";
 module.exports.LISTEN_TO_PROJECT = "listen-to-project";
+module.exports.BROADCAST_TO_PROJECT = "broadcast-to-project";
