7d5b9abb05dc2e2be848fa86852915cd34b488dd
Devesh Kumar <devesh2027@gmail.com>
2022-12-17 00:18:07 +0530

    backend: Fixed SSH into instance and starting of real-time app

--- backend/utils/rce/copyFilesAndStartAppOnInstance.js
+++ backend/utils/rce/copyFilesAndStartAppOnInstance.js
@@ -23,7 +23,7 @@ const copyFilesAndStartAppOnInstance = async (
 		await ssh.connect({
 			host: instance.PublicIpAddress,
 			username: "ec2-user",
-			privateKey: process.env.AWS_EC2_RUNNER_SSH_KEY_CONTENT,
+			privateKey: require("./ssh-key"),
 		});
 
 		// SSH Connection established, now copy files for the project to the instance.
@@ -34,7 +34,15 @@ const copyFilesAndStartAppOnInstance = async (
 			return { error: new Error("No files present in the project") };
 
 		// Run Git Clone to get app-runner files.
-		await ssh.execCommand(`git clone ${process.env.REPO_GITHUB_CLONE_PATH} ./`);
+		await ssh.execCommand(
+			`git clone ${process.env.REPO_GITHUB_CLONE_PATH} ./repo`
+		);
+		// Copy .env file from app-runner directory to app-runner on ec2 instance
+		const path = require("path");
+		await ssh.putFile(
+			path.resolve(process.cwd(), "../app-runner/.env"),
+			"repo/app-runner/.env"
+		);
 
 		// Project files to copy.
 		const filesToCopy = [];
@@ -42,7 +50,8 @@ const copyFilesAndStartAppOnInstance = async (
 		for (const file of projectFiles) {
 			// Create these project files on the server temporarily
 			const fileTempPath = "/temp/" + projectId + file.path;
-			const fileOnRunnerServerMatchingPath = "project-app/" + file.path;
+			const fileOnRunnerServerMatchingPath =
+				"repo/app-runner/project-app/" + file.path;
 
 			fileCreationPromises.push(fse.outputFile(fileTempPath, file.contents));
 			filesToCopy.push({
@@ -69,9 +78,8 @@ const copyFilesAndStartAppOnInstance = async (
 
 		// Run the app just created on the remote runner server.
 		// via the app-runner we copied to the instance.
-		await ssh.execCommand("npm install"); // Install dependencies for the app-runner
-		await ssh.execCommand(
-			`node ./app-runner.js "${projectId}" "${installCommand}" "${startCommand}"`
+		ssh.execCommand(
+			`cd repo/app-runner && nvm use 16.10.0 && npm install && node ./index.js "${projectId}" "${installCommand}" "${startCommand}"`
 		);
 
 		return {};
