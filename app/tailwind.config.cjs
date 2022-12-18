a63dd89213b62028b6d1fbf47dac7287ab47fdef
Devesh Kumar <devesh2027@gmail.com>
2022-12-18 21:52:43 +0530

    app: Updated tailwind config

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		textColor: {
+			white: "#FFFFFF",
+		},
+		colors: {
+			stone: colors.stone,
+			sky: colors.sky,
+			neutral: colors.neutral,
+			gray: colors.gray,
+			slate: colors.slate,
+			black: colors.black,
+			editor: "#263238",
+			editorpane: "#246b67",
+			transparent: "transparent",
+		},
 	},
 	plugins: [],
 };
