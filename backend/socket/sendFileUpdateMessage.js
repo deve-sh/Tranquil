8572e6674640025e6693bceb62b676f12c504703
Devesh Kumar <devesh2027@gmail.com>
2022-12-18 12:38:39 +0530

    backend: fixed emitting of file update ping for app-runner

--- backend/socket/sendFileUpdateMessage.js
+++ backend/socket/sendFileUpdateMessage.js
@@ -6,11 +6,9 @@ const { FILE_UPDATED } = require("../../common/socketTypes");
 
 const sendFileUpdateMessage = (projectId, fileUpdatePayload) => {
 	// Notify App Runner that a file has been updated.
-	socketServer.to(
-		getAppRunnerSocketRoomId(projectId),
-		FILE_UPDATED,
-		fileUpdatePayload
-	);
+	socketServer
+		.to(getAppRunnerSocketRoomId(projectId))
+		.emit(FILE_UPDATED, fileUpdatePayload);
 
 	// Send signal that file has been updated to all project connected sockets.
 	sendMessageToProjectSocketRoom(projectId, FILE_UPDATED, fileUpdatePayload);
