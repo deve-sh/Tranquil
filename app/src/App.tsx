5b8de38ed426f81ae1a94ddec1d7663140c0164e
Devesh Kumar <devesh2027@gmail.com>
2022-12-18 20:22:19 +0530

    app: Created POC for Editor using CodeMirror

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import EditorBase from "./Project/Editor/Base";
+
 function App() {
-	return <></>;
+	const [code, setCode] = useState(`function add(x, y){ return x + y; }`);
+
+	return <EditorBase code={code} onChange={(_, __, value) => setCode(value)} />;
 }
 
 export default App;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,7 +5,5 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<App />
 );
