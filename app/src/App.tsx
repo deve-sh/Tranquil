bd215fcf1b54ff9ab5a22f0ba44df168288f12d7
Devesh Kumar <devesh2027@gmail.com>
2022-12-20 11:11:26 +0530

    app: Fixed router wrapper issues

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Toast from "./Components/Layout/Toast";
 import ProjectEditor from "./Components/Project/Editor";
@@ -10,34 +10,36 @@ function App() {
 	return (
 		<>
 			<Toast />
-			<Route
-				path="/createproject"
-				element={
-					<Suspense fallback={<></>}>
-						<CreateProject />
-					</Suspense>
-				}
-			/>
-			<Route
-				path="/project/:projectId"
-				element={
-					<Suspense fallback={<></>}>Project Page. Coming soon</Suspense>
-				}
-			/>
-			<Route
-				path="/project/:projectId/editor"
-				element={
-					<Suspense fallback={<></>}>
-						<ProjectEditor />
-					</Suspense>
-				}
-			/>
-			<Route
-				path="/dashboard"
-				element={
-					<Suspense fallback={<></>}>User Dashboard. Coming soon</Suspense>
-				}
-			/>
+			<Routes>
+				<Route
+					path="/createproject"
+					element={
+						<Suspense fallback={<></>}>
+							<CreateProject />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/project/:projectId"
+					element={
+						<Suspense fallback={<></>}>Project Page. Coming soon</Suspense>
+					}
+				/>
+				<Route
+					path="/project/:projectId/editor"
+					element={
+						<Suspense fallback={<></>}>
+							<ProjectEditor />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/dashboard"
+					element={
+						<Suspense fallback={<></>}>User Dashboard. Coming soon</Suspense>
+					}
+				/>
+			</Routes>
 		</>
 	);
 }
