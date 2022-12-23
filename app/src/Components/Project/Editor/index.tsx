b04a345b10d815cbac724e36a5ae9787790a777f
Devesh Kumar <devesh2027@gmail.com>
2022-12-23 23:57:45 +0530

    app: Added fetching of file list and file contents for project editor
    Temporarily disabled instance state updates for testing

--- app/src/Components/Project/Editor/index.tsx
+++ app/src/Components/Project/Editor/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { useParams } from "react-router-dom";
+import { getProjectFileContent, getProjectFileList } from "../../../API/Files";
 import { initializeProjectRCE } from "../../../API/RCE";
 
 import {
@@ -15,18 +16,22 @@ import ProjectTerminalOutput from "./ProjectTerminalOutput";
 const ProjectEditor = () => {
 	const { projectId } = useParams();
 
-	const [code, setCode] = useState(`function add(x, y){ return x + y; }`);
-	const [projectAppInstanceURL, setProjectAppInstanceURL] = useState("");
+	// Project Execution Iframe
 	const iframeRef = useRef<HTMLIFrameElement | null>(null);
+	const [projectAppInstanceURL, setProjectAppInstanceURL] = useState("");
 
 	const reloadIframe = useCallback(() => {
 		if (iframeRef.current) iframeRef.current.src = iframeRef.current.src;
 	}, []);
 
+	// File List and active file info
+	const [fileList, setFileList] = useState([]);
+	const [activeFileId, setActiveFileId] = useState("");
+	const [code, setCode] = useState("");
+
 	// Project Terminal Output
 	const [showAppTerminal, setShowAppTerminal] = useState(false);
 	const [appTerminalLogs, setAppTerminalLogs] = useState<string[]>([]);
-
 	const toggleProjectTerminal = () => setShowAppTerminal((show) => !show);
 
 	const onProjectSocketEvent = useCallback(
@@ -58,6 +63,28 @@ const ProjectEditor = () => {
 		[projectId]
 	);
 
+	const getFileListAndSetLastUpdatedFile = useCallback(async () => {
+		if (!projectId) return;
+
+		const { data: response } = await getProjectFileList(projectId);
+		if (!response.fileList) return;
+
+		const fileList = response.fileList;
+		setFileList(fileList);
+
+		// Find the last updated file from the list and set that as the active file.
+		let latestUpdatedFile = null;
+		for (const fileEntry of fileList) {
+			if (latestUpdatedFile === null) {
+				latestUpdatedFile = fileEntry;
+				continue;
+			}
+			if (new Date(fileEntry.updatedAt) > new Date(latestUpdatedFile.updatedAt))
+				latestUpdatedFile = fileEntry;
+		}
+		setActiveFileId(latestUpdatedFile._id);
+	}, [projectId]);
+
 	useEffect(() => {
 		if (projectId) {
 			// Initialize socket connection to project room.
@@ -65,15 +92,25 @@ const ProjectEditor = () => {
 			setupProjectEventListeners(onProjectSocketEvent);
 
 			// Send an initialization API Call for the project.
-			initializeProjectRCE(projectId).then(({ data: response }) => {
-				if (response.publicURL) setProjectAppInstanceURL(response.publicURL);
-			});
+			// initializeProjectRCE(projectId).then(({ data: response }) => {
+			// 	if (response.publicURL) setProjectAppInstanceURL(response.publicURL);
+			// });
+
+			getFileListAndSetLastUpdatedFile();
 
 			return () => {
 				disconnectProjectSocket(projectId);
 			};
 		}
 	}, [projectId]);
+
+	useEffect(() => {
+		if (activeFileId && projectId) {
+			getProjectFileContent(projectId, activeFileId).then(
+				({ data: contents }) => setCode(contents)
+			);
+		} else setCode("");
+	}, [activeFileId]);
 
 	return (
 		<div className="project-editor flex w-full h-screen">
