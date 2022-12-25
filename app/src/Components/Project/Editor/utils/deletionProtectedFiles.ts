aaf228fc41e4d4039b7536dbde15d2bdadba3c39
Devesh Kumar <devesh2027@gmail.com>
2022-12-25 12:46:37 +0530

    app: bug: Fixed deletion protection of non-protected files

--- app/src/Components/Project/Editor/utils/deletionProtectedFiles.ts
+++ app/src/Components/Project/Editor/utils/deletionProtectedFiles.ts
@@ -2,7 +2,7 @@ const deletionProtectedFiles: string[] = ["package.json"];
 
 const isDeletionProtectedFile = (filePath: string) => {
 	for (const file of deletionProtectedFiles)
-		if (filePath.includes(filePath)) return true;
+		if (file.includes(filePath)) return true;
 	return false;
 };
 
