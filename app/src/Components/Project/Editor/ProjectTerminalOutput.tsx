c124e1021055387070cc5e4a387a017642ad1234
Devesh Kumar <devesh2027@gmail.com>
2022-12-29 14:39:09 +0530

    app: Added terminal closing icon and CTA

--- app/src/Components/Project/Editor/ProjectTerminalOutput.tsx
+++ app/src/Components/Project/Editor/ProjectTerminalOutput.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { MdClose } from "react-icons/md";
 
 interface Props {
 	logs: string[];
@@ -23,6 +24,16 @@ const ProjectTerminalOutput = ({ logs, isOpen, toggle }: Props) => {
 				isOpen ? "" : "translate-y-full"
 			}`}
 		>
+			<a
+				href="#"
+				onClick={(e) => {
+					e.preventDefault();
+					toggle();
+				}}
+				className="absolute top-4 right-4 text-white text-lg"
+			>
+				<MdClose />
+			</a>
 			<div
 				className={`project-terminal
                 bg-black
