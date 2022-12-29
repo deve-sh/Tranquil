da22ec0a6a749689a48f8531004d9bbc9ad96f7b
Devesh Kumar <devesh2027@gmail.com>
2022-12-29 18:24:35 +0530

    app-runner: Added back auth token setter for ngrok

--- app-runner/createHTTPSTunnel.js
+++ app-runner/createHTTPSTunnel.js
@@ -2,7 +2,7 @@ const ngrok = require("ngrok");
 
 const createHTTPSTunnel = async (port = 3000) => {
 	try {
-		// await ngrok.authtoken(process.env.NGROK_AUTH_TOKEN);
+		await ngrok.authtoken(process.env.NGROK_AUTH_TOKEN);
 		const url = await ngrok.connect(port);
 		return { url };
 	} catch (error) {
